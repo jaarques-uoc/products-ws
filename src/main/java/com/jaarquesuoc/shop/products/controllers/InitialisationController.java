86b40c72819d7cd03d8c2f38c9c498873336d0f7
Jordi <jaarques@uoc.edu>
2019-05-22 22:01:44 +0200

    Locate products.json to the resources folder

--- src/main/java/com/jaarquesuoc/shop/products/controllers/InitialisationController.java
+++ src/main/java/com/jaarquesuoc/shop/products/controllers/InitialisationController.java
@@ -6,11 +6,10 @@ import com.jaarquesuoc.shop.products.dtos.ProductDto;
 import com.jaarquesuoc.shop.products.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
@@ -36,8 +35,7 @@ public class InitialisationController {
 
     private List<ProductDto> buildProducts() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        File initialFile = new File("products.json");
-        InputStream targetStream = new FileInputStream(initialFile);
+        InputStream targetStream = new ClassPathResource("products.json").getInputStream();
 
         return Arrays.asList(mapper.readValue(targetStream, ProductDto[].class));
     }
